1ae29b1c84c4cd56d52f85c8d7c8569db06bbd9f
shirogaku <shirogaku@gmail.com>
2020-06-16 00:12:54 +0900

    Initial commit

---
+++ wifi.lua
@@ -0,0 +1,32 @@
+--[[
+	This will be called by init.lua
+]]
+
+-- Constant
+WIFI_CFG = {}
+WIFI_CFG.ssid = ""
+WIFI_CFG.pwd = ""
+WIFI_CFG.save = false
+WIFI_IS_AUTOCONNECT = 1
+
+-- Flag
+IS_CONNECTION_OK = false
+
+-- WIFI event monitor when connected
+function on_wifi_connected(T)
+	print("WIFI connected to "..T.SSID.." on channel "..T.channel)
+	IS_CONNECTION_OK = true
+end
+
+-- WIFI do autoconnect
+wifi.sta.autoconnect(WIFI_IS_AUTOCONNECT)
+
+-- WIFI set config
+wifi.sta.config(WIFI_CFG)
+
+-- WIFI connect
+wifi.sta.connect(on_wifi_connected)
+print("WIFI CONNECTING")
+
+-- Wait until get ip
+while IS_CONNECTION_OK == false do end
